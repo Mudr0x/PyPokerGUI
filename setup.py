3ea31a2722660574c977d4a014856903879009e1
Tom Short <tom.short@themindlab.co.uk>
2021-09-15 12:16:20 +0100

    tweaked setup file

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'PyPokerGUI',
+    name = 'PyPokerGUI_tomtest',
     version = '0.0.7',
     author = 'ishikota',
     author_email = 'ishikota086@gmail.com',
     description = 'GUI application for PyPokerEngine',
     license = 'MIT',
     keywords = 'python poker engine gui',
-    url = 'https://github.com/ishikota/PyPokerGUI',
+    url = 'https://github.com/tomS-mindlab/PyPokerGUI_tomtest',
     packages = [pkg for pkg in find_packages() if pkg != "tests"],
     package_data={
-        'pypokergui': [
+        'pypokergui_tomtest': [
             'server/static/*.css',
             'server/static/*.js',
             'server/static/images/*',
@@ -23,7 +23,6 @@ setup(
         "License :: OSI Approved :: MIT License",
     ],
     install_requires=[
-        'pypokerengine',
         'tornado==4.4.2',
         'click==6.7',
         'PyYAML==3.12',
